35573431864c607c7c7032ffdd57fd6babe53ec2
Mohamed Achref <masakkat@naxxum.fr>
2019-10-27 12:20:12 +0100

    test 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel fffff
                 </div>
 
                 <div class="links">
