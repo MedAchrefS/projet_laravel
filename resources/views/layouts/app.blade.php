d666a98994c04ed2947cbceab42b3e30b60eda55
Mohamed Achref <masakkat@naxxum.fr>
2019-11-23 11:55:53 +0100

    leaflet config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,15 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <script src="{{ asset('js/app.js') }}" ></script>
+    <script src="{{ asset('leaflet/leaflet.js') }}" ></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('leaflet/leaflet.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
