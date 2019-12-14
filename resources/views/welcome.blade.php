612ca95026969f1b4784fe19be02f73cc418cc25
rafikhechmi <rafikhechmi@gmail.com>
2019-12-14 08:02:15 -0800

    welcomblade
    :+1:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 
-<!DOCTYPE HTML>
+
 <html>
 <head>
 <title>Travel Home</title>
@@ -289,10 +289,7 @@ Smartphone Compatible web template, free web designs for Nokia, Samsung, LG, Son
 
 	<!-- //footer-section -->
       <div class="map-bottom">
-					<div class="col-md-4 adrs-left wow fadeInUp animated animated" data-wow-delay=".5s">
-						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
-                        <p> {{ Auth::user()->adresse }}</p>
-					</div>
+
 					<div class="col-md-4 adrs-left adrs-middle wow fadeInUp animated animated" data-wow-delay=".5s">
                         <div class="content">
 
@@ -316,11 +313,7 @@ Smartphone Compatible web template, free web designs for Nokia, Samsung, LG, Son
 
 
 					</div>
-					<div class="col-md-4 adrs-left adrs-right wow fadeInUp animated animated" data-wow-delay=".5s">
-						<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
-                            <p> {{ Auth::user()->email }}</p>
 
-					</div>
 					<div class="clearfix"></div>
 				</div>
 			<!--//map-->
